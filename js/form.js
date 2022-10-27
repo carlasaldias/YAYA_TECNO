a6d23881b3fe2d1c9ca1a7f33c435b19b2780e74
Marco Silva <nelsonmarcosilva@gmail.com>
2022-10-26 23:22:26 -0300

    agregado de formu y mapa

--- js/form.js
+++ js/form.js
@@ -2,10 +2,12 @@ let inpName = document.getElementById("fname")
 let labelName = document.getElementById("nameController")
 let inpLastName = document.getElementById("lname")
 let labelLastName = document.getElementById("lnameController")
+let inpEmail = document.getElementById("email")
+let labelEmail = document.getElementById("emailController")
 let inpMsg = document.getElementById("subject")
 let labelMsg = document.getElementById("subjectController")
-let inpArray = [inpName, inpLastName,inpMsg];
-let labelArray = [labelName, labelLastName, labelMsg];
+let inpArray = [inpName, inpLastName, inpEmail, inpMsg];
+let labelArray = [labelName, labelLastName, labelEmail, labelMsg];
 let form = document.getElementById("form");
 let flag = false;
 
@@ -25,13 +27,29 @@ for (let i = 0; i < inpArray.length; i++) {
 
     inpArray[i].addEventListener("change", (e) => {
 
-        if (inpArray[i].value.length < 5 && inpArray[i].value.length>0) {
+        if (inpArray[i].value.length < 5) {
             errorFunction(i);
         } else {
             validFuction(i);
         }
+        if (inpArray[i].value.length === 0) {
+            validFuction(i);
+        }
 
     })
+    inpArray[2].addEventListener("change", (e) => {
+        if (inpArray[2].value.length != 0) {
+            if ((!inpArray[2].value.includes("@") || !inpArray[2].value.includes(".com"))
+            ) {
+                errorFunction(2);
+            }
+        } else {
+            validFuction(2)
+        }
+        ;
+
+    })
+
     form.addEventListener("submit", (e) => {
         if (inpArray[i].value == "" || inpArray[i].value == " ") {
             errorFunction(i);
